32b356869644fd5db3f89ccb18cfc12ef70b8912
GabWiz <rogi.mgi08@gmail.com>
2024-12-10 08:10:18 -0300

    Add files via upload

---
+++ Variáveis e Operações Básicas.py
@@ -0,0 +1,9 @@
+num1 = float(input("DIgite o primeiro número: "))
+num2 = float(input("Digite o segundo número: "))
+
+soma = num1 + num2
+subtracao = num1 - num2
+divisao = num1 / num2
+multiplicacao = num1 * num2
+
+print(f"Soma: {soma}, Subtração: {subtracao}, Divisão: {divisao}, Multiplicação, {multiplicacao}. ")
